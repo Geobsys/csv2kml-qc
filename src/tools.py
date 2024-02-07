e6a75cb1555ad565d7a74310a396ea17ce6ea059
Falawiss <138690251+Falawiss@users.noreply.github.com>
2024-02-07 16:36:43 +0100

    switch to pandas

--- src/tools.py
+++ src/tools.py
@@ -3,40 +3,25 @@
 @author: mehdi daakir
 """
 
-import csts,os #simplekml
+import csts,os,simplekml
 import numpy as np
 import pandas as pd
 from pyproj import Transformer
 
-#création du dataframe à partir du csv
-colName = ["time", "day", "state", "lat", "lon", "h", "incert_lat", "incert_lon", "oX", "oY", "oZ"]
-df = pd.read_csv("../test/EXTENVENT.LOG", delimiter=",", header=None)
-df.columns = colName
-
-#transformation des coordonnées (EPSG:432 vers EPSG:4964 (XYZ)) et stockage dans un autre dataframe
-transformer = Transformer.from_crs(4326, 4964)
-coordLambert = transformer.transform(df['lat'], df['lon'], df['h'])
-coordLambert = np.array(coordLambert)
-
-df2 = pd.DataFrame(coordLambert.T, columns=['coordX', 'coordY', 'coordZ'])
-
-# #Fusion des deux dataframes
-df = pd.concat([df, df2], axis=1)
-
 
 def custom_pt(
-              kml,
-              lon,
-              lat,
-              h,
-              status="None",
-              mode="icon",
-              name="",
-              description="",
-              label_scale=2,
-              icon_scale=1,
-              icon_href="http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png"
-             ):
+			  kml,
+			  lon,
+			  lat,
+			  h,
+			  status="None",
+			  mode="icon",
+			  name="",
+			  description="",
+			  label_scale=2,
+			  icon_scale=1,
+			  icon_href="http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png"
+			 ):
 
 	if(mode=="icon"):
 		#append a pt
@@ -55,32 +40,27 @@ def custom_pt(
 	return None
 
 def csv_to_kml(
-               input_file,
-               output_file="",
-               separator=",",
-               doc_name="",
-               print_stats=True,
-               mode="icon",
-               label_scale=2,
-               icon_scale=1,
-               icon_href="http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png"
-              ):
-
+			   input_file,
+			   output_file="",
+			   separator=",",
+			   doc_name="",
+			   print_stats=True,
+			   mode="icon",
+			   label_scale=2,
+			   icon_scale=1,
+			   icon_href="http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png"
+			  ):
 	#my data
-	data=[]
-
-	#load input file
-	f = open(input_file, 'r')
-	for line in f:
-		data.append(line.strip().split(separator))
-	f.close()
-
+	labels = ["time", "day", "state", "lat", "lon", "h", "incert_lat", "incert_lon", "oX", "oY", "oZ"]
+	data = pd.read_csv(input_file)
+	data.columns = labels
+	
 	#show some statistics
 	if(print_stats):
 		print("(#) samples \t = %d" % len(data))
-		print("(#) %s \t = %d (%.1f%%)" % (csts.status_dict["R"]["name"],[elem[2] for elem in data].count("R"),100*[elem[2] for elem in data].count("R")/len(data)))
-		print("(#) %s \t = %d (%.1f%%)" % (csts.status_dict["F"]["name"],[elem[2] for elem in data].count("F"),100*[elem[2] for elem in data].count("F")/len(data)))
-		print("(#) %s \t = %d (%.1f%%)" % (csts.status_dict["N"]["name"],[elem[2] for elem in data].count("N"),100*[elem[2] for elem in data].count("N")/len(data)))
+		print("(#) %s \t = %d (%.1f%%)" % (csts.status_dict["R"]["name"],[elem[2] for elem in data.values].count("R"),100*[elem[2] for elem in data.values].count("R")/len(data)))
+		print("(#) %s \t = %d (%.1f%%)" % (csts.status_dict["F"]["name"],[elem[2] for elem in data.values].count("F"),100*[elem[2] for elem in data.values].count("F")/len(data)))
+		print("(#) %s \t = %d (%.1f%%)" % (csts.status_dict["N"]["name"],[elem[2] for elem in data.values].count("N"),100*[elem[2] for elem in data.values].count("N")/len(data)))
 
 	#instance class
 	kml=simplekml.Kml()
@@ -88,11 +68,22 @@ def csv_to_kml(
 	#assign a name
 	if(doc_name==""): doc_name=os.path.basename(input_file)
 	kml.document.name = doc_name
-
+	
 	#assign a name
 	if(output_file==""): output_file="".join([os.path.splitext(input_file)[0],".kml"])
 
-    # Calculate time between to measures
+		
+	# Coordinates transformation to cartesian geocentric 
+	transformer = Transformer.from_crs(4326, 4964)
+	coordLambert = transformer.transform(data['lat'], data['lon'], data['h'])
+	coordLambert = np.array(coordLambert)
+	
+	data[['coordX', 'coordY', 'coordZ']] = coordLambert.T.round(3)
+
+	# Calculate distance between two points
+	data["dist"] = np.sqrt(data["coordX"].diff()**2 + data["coordY"].diff()**2 + data["coordZ"].diff()**2).round(3)
+
+	# Calculate time between to measures
 	data["time_laps"] = data["time"].diff().round(3)
 	data.loc[0,"time_laps"] = 0.
 	
@@ -136,22 +127,22 @@ def gen_description(pt) :
 	return text
 
 def get_dist_point_to_point(dataframe, index):  
-    return  (dataframe.coordX[index] - dataframe.coordX[index-1])**2 + (dataframe.coordY[index] - dataframe.coordY[index-1])**2 + (dataframe.coordZ[index] - dataframe.coordZ[index-1])**2
-    
+	return  np.sqrt(dataframe.coordX[index].diff()**2 + dataframe.coordY[index].diff()**2 + dataframe.coordZ[index].diff()**2)
+	
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
